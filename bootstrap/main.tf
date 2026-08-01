d70396fbf7d459092c711282cdb4ea4e7bf2ff61
Sandali <sandalichandrasekarat20@gmail.com>
2026-08-01 14:57:59 +0530

    Fix formatting

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -37,6 +37,6 @@ resource "aws_s3_bucket_public_access_block" "state" {
 
   block_public_acls       = true
   block_public_policy      = true
-  ignore_public_acls       = true
-  restrict_public_buckets  = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
